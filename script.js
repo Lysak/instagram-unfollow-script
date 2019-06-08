5ee30e1e8e7e86b608c8211bf9f20b713b79c06c
Dmytrii Lysak <siefan95@gmail.com>
2019-06-08 17:45:23 +0300

    update

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
-let buttonTags = document.getElementsByTagName("button");
-let count = 1;
-let buttonTagsCancel = document.getElementsByTagName("button");
-let searchText = "Отменить подписку";
-let found;
-let i;
-let j;
+if (typeof buttonTags === 'undefined') {
+    let buttonTags, count, buttonTagsCancel, searchText, found;
+}
 
 buttonTags = document.getElementsByTagName("button");
 count = 1;
-for (i = 11; i < buttonTags.length; i++) {
+buttonTagsCancel = document.getElementsByTagName("button");
+searchText = "Отменить подписку";
+
+for (let i = 7; i < buttonTags.length; i++) {
     (function (i) {
-        let timeToStartNote = 29000 * count;
+        let timeToStart = (count === 1 ? 1000 : 44000) * count;
         setTimeout(function () {
             buttonTags[i].click();
-            console.log('https://lysak.github.io/' + ' - ' + i);
-        }, timeToStartNote);
+            if (i % 10) {
+                console.log('https://lysak.github.io/' + ' - ' + i);
+            }
+            
+        }, timeToStart);
 
         setTimeout(function () {
             buttonTagsCancel = document.getElementsByTagName("button");
-            for (j = 0; j < buttonTagsCancel.length; j++) {
+            for (let j = 0; j < buttonTagsCancel.length; j++) {
                 if (buttonTagsCancel[j].textContent == searchText) {
                     found = buttonTagsCancel[j];
                     buttonTagsCancel[j].click();
                     break;
                 }
             }
-			console.log('unfollow');
-        }, +timeToStartNote + 1000);
+			console.log('unfollow ' + new Date().toISOString().replace(/T|Z|\.\d{3}/g, ' ').trim());
+        }, +timeToStart + 1000);
     })(i);
     count++;
 }
